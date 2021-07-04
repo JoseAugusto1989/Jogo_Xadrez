11b519df9538f4e636b927a6ce1b48fec095e3ea
Jose Augusto de Oliveira Pins <64021880+JoseAugusto1989@users.noreply.github.com>
2021-07-04 20:14:44 -0300

    atualizando o metodo para impressão das peças

---
+++ src/boardGame/BoardException.java
@@ -0,0 +1,11 @@
+package boardGame;
+
+public class BoardException extends RuntimeException {
+
+	private static final long serialVersionUID = 1254397875508152823L;
+
+	public BoardException (String msg) {
+		super(msg);
+	}
+	
+}
